1e5c81c1baec0befa6cb20f9ee50aec01652d085
NarwhalTheGreat <92124540+NarwhalTheGreat@users.noreply.github.com>
2026-04-02 11:40:42 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,144 +1,31 @@
-import pandas as pd
-from sqlalchemy import create_engine, text
+import report_by_event
+import report_by_dates
 
-#STEP1---------------------------------------
-# Connect to the database
-engine = create_engine('postgresql+psycopg2://postgres:admin@localhost:5432/witisi-ordering-local')
+print("what would you like to do? \n 1 for Report by event \n 2 for Report by date")
+option = input()
+print("Please provide the hostname")
+hostname = input()
+print("Please provide your credentials for accessing the database")
+print("Username:")
+username = input()
+print("Password:")
+password = input()
 
-# Establish and check Connection
-try:
-    connection = engine.connect()
-    print("Connection to PostgreSQL witisi-ordering established successfully!")
-except Exception as e:
-    print(f"Connection failed! Error: {e}")
+if (option == "1"):
+    print("Catalogue ID:")
+    # '7ef69632-0ff6-48ba-b0d2-c53b5c4a7c7c'
+    catalog_id = input()
+    print("Catalogue Name:")
+    catalog_name = input()
+    report_by_event.report_by_event(username, password, catalog_id, catalog_name, hostname)
 
-# ── Define the CatalogId dynamically ─────────────────────────────────────────
-catalog_id = '7ef69632-0ff6-48ba-b0d2-c53b5c4a7c7c'
+elif (option == "2"):
+    print("Date Start (inclusive) (yyyy-mm-dd):")
+    # '7ef69632-0ff6-48ba-b0d2-c53b5c4a7c7c'
+    date_from = input()
+    print("Date End (exclusive) (yyyy-mm-dd):")
+    date_to = input()
+    report_by_dates.report_by_dates(username, password, date_from, date_to, hostname)
 
-#Query n1
-query1 = text("""
--- DB host: 10.10.2.42
--- DB: witisi-ordering
-    SELECT  o."OrderId",
-            o."Number",
-            o."Email",
-            o."CreatedOn",
-            o."Amount",
-            o."Discount",
-            '(''' || o."OrderId" || ''', ''' || o."Number" || ''', ''' || o."Email" || ''', ''' || o."CreatedOn" || ''', ''' || o."Amount" || ''', ''' || o."Discount" || '''),' as "OrderInfo"
-    FROM "Order" o 
-    JOIN "OrderItem" oi ON oi."OrderId" = o."OrderId" 
-    JOIN "Catalog" c ON c."CatalogId" = oi."CatalogId" 
-    WHERE oi."CatalogId" = :catalog_id
-    AND o."State" = 'Completed'
-    GROUP BY o."OrderId",
-            o."Number",
-            o."Email",
-            o."CreatedOn",
-            o."Amount",
-            o."Discount"
-""")
-
-# Run query 1
-eventOrderDf1 = pd.read_sql(query1.bindparams(catalog_id=catalog_id), connection)
-
-# All orders in a string for the next query
-all_orders = '\n'.join(eventOrderDf1['OrderInfo'].astype(str))
-
-# Remove trailing comma
-all_orders = all_orders[:-1]
-
-#DEBUGS___________________________
-print(eventOrderDf1)
-
-#print(all_orders)
-
-#STEP2 ---------------------------------------
-#Query n2
-# Connect to the database
-engine2 = create_engine('postgresql+psycopg2://postgres:admin@localhost:5432/witisi-invoicing-local')
-
-# Establish and check Connection
-try:
-    connection2 = engine2.connect()
-    print("Connection to PostgreSQL witisi-invoicing established successfully!")
-except Exception as e:
-    print(f"Connection failed! Error: {e}")
-
-query2 = text(f"""
--- DB host: 10.10.2.42
--- DB: witisi-invoicing
-SELECT  o."OrderId",
-		o."Number",
-		o."Email",
-		o."CreatedOn",
-		p."PaymentId",
-		o."Amount",
-		o."Discount",
-		p."Amount" as "PaymentAmount",
-		'(''' || o."OrderId" || ''', ''' || o."Number" || ''', ''' || o."Email" || ''', ''' || o."CreatedOn" || ''', ''' || p."PaymentId" || ''', ''' || o."Amount" || ''', ''' || o."Discount" || ''', ''' || p."Amount" || '''),' as "OrderInfo"
-FROM (VALUES
--- data begin
-    {all_orders}
--- data end
-) as o("OrderId", "Number", "Email", "CreatedOn", "Amount", "Discount")
-join "Payment" p on p."InvoiceId"::text = o."OrderId"
-where p."State" = 'Accepted'
-and p."GatewayId" = 'stripe'
-""")
-
-# Run query 2
-df2 = pd.read_sql(query2, connection2)
-df2.to_csv("data/data2.csv")
-
-# All orders in a string for the next query
-all_accepted_orders = '\n'.join(df2['OrderInfo'].astype(str))
-
-# Remove trailing comma
-all_accepted_orders = all_accepted_orders[:-1]
-
-#STEP3 ---------------------------------------
-#Query n3
-# Connect to the database
-engine3 = create_engine('postgresql+psycopg2://postgres:admin@localhost:5432/witisi-stripe-payment-local')
-
-# Establish and check Connection
-try:
-    connection3 = engine3.connect()
-    print("Connection to PostgreSQL witisi-stripe established successfully!")
-except Exception as e:
-    print(f"Connection failed! Error: {e}")
-
-# Establish even info
-event_name = 'Piraeus Port Run 2026'
-
-query3 = text(f"""
--- DB host: 10.10.2.42
--- DB: witisi-stripe
-
-select  o."OrderId",
-		o."Number",
-		o."Email",
-		o."CreatedOn",
-		o."PaymentId",
-		replace(o."Amount"::text, '.', ',') as "OrderAmount",
-		replace(o."Discount"::text, '.', ',') as "OrderDiscount",
-		p."CurrencyId",
-		p2."IssuerCountry" as "Country",
-		p3."PaymentIntentId",
-		'{event_name}' as "AlbumTitle",
-		replace(p."Amount"::text, '.', ',') as "PaymentAmount"
-from (values
--- data begin
-    {all_accepted_orders}
--- data end
-	 ) as o("OrderId", "Number", "Email", "CreatedOn", "PaymentId", "Amount", "Discount", "PaymentAmount")
-		 join "Payment" p on p."PaymentId"::text = o."PaymentId"
-		 join "CardPayment" p2 on p."PaymentId" = p2."PaymentId"
-		 join "ChargedPayment" p3 on p."PaymentId" = p3."PaymentId"
-""")
-
-# Run query 2
-df3 = pd.read_sql(query3, connection3)
-df3.to_csv("data/data3.csv", index=False)
-print(df3)
+else:
+    print("Invalid Input")
---
+++ report_by_dates.py
@@ -0,0 +1,148 @@
+import pandas as pd
+from numpy.f2py.auxfuncs import throw_error
+from sqlalchemy import create_engine, text
+
+def report_by_dates(username, password, date_from, date_to, hostname):
+
+
+    # Connect to the databases
+    port = '6432'
+    #engine_ordering = create_engine('postgresql+psycopg2://postgres:admin@localhost:5432/witisi-ordering-local')
+    engine_ordering = create_engine(f'postgresql+psycopg2://{username}:{password}@{hostname}:{port}/witisi-ordering')
+
+    # Establish and check Connection
+    try:
+        connection_ordering = engine_ordering.connect()
+        print("Connection to PostgreSQL witisi-ordering established successfully!")
+    except Exception as e:
+        print(f"Connection failed! Error: {e}")
+        raise ValueError(f"Connection failed! Error: {e}")
+
+    engine_invoicing = create_engine(f'postgresql+psycopg2://{username}:{password}@{hostname}:{port}/witisi-invoicing')
+
+    try:
+        connection_invoicing = engine_invoicing.connect()
+        print("Connection to PostgreSQL witisi-invoicing established successfully!")
+    except Exception as e:
+        print(f"Connection failed! Error: {e}")
+        raise ValueError(f"Connection failed! Error: {e}")
+
+    engine_stripe = create_engine(f'postgresql+psycopg2://{username}:{password}@{hostname}:{port}/witisi-stripe')
+
+    try:
+        connection_stripe = engine_stripe.connect()
+        print("Connection to PostgreSQL witisi-stripe established successfully!")
+    except Exception as e:
+        print(f"Connection failed! Error: {e}")
+        raise ValueError(f"Connection failed! Error: {e}")
+
+    # Query n1
+    query1 = text("""
+    SELECT o."OrderId",
+           o."Number",
+           o."Email",
+           o."CreatedOn",
+           o."Amount",
+           o."Discount",
+           '(''' || o."OrderId" || ''', ''' || o."Number" || ''', ''' || o."Email" || ''', ''' ||
+           o."CreatedOn" || ''', ''' || o."Amount" || ''', ''' || o."Discount" || '''),' as "OrderInfo"
+    FROM "Order" o
+             JOIN "OrderItem" oi ON oi."OrderId" = o."OrderId"
+             JOIN "Catalog" c ON c."CatalogId" = oi."CatalogId"
+    WHERE o."CreatedOn" >= :date_from
+      AND o."CreatedOn" <  :date_to
+      AND o."State" = 'Completed'
+    GROUP BY o."OrderId",
+             o."Number",
+             o."Email",
+             o."CreatedOn",
+             o."Amount",
+             o."Discount"
+                  """)
+
+    # Run query 1
+    df1 = pd.read_sql(query1.bindparams(date_from=date_from, date_to=date_to), connection_ordering)
+    connection_ordering.close()
+    engine_ordering.dispose()
+
+    # All orders in a string for the next query
+    all_orders = '\n'.join(df1['OrderInfo'].astype(str))
+
+    # Remove trailing comma
+    all_orders = all_orders[:-1]
+
+    # print(all_orders)
+
+    # STEP2 ---------------------------------------
+    # Query n2
+    query2 = text(f"""
+-- DB host: 10.10.2.42
+-- DB: witisi-invoicing
+SELECT  o."OrderId",
+		o."Number",
+		o."Email",
+		o."CreatedOn",
+		p."PaymentId",
+		o."Amount",
+		o."Discount",
+		p."Amount" as "PaymentAmount",
+		'(''' || o."OrderId" || ''', ''' || o."Number" || ''', ''' || o."Email" || ''', ''' || o."CreatedOn" || ''', ''' || p."PaymentId" || ''', ''' || o."Amount" || ''', ''' || o."Discount" || ''', ''' || p."Amount" || '''),' as "OrderInfo"
+FROM (VALUES
+-- data begin
+    {all_orders}
+-- data end
+) as o("OrderId", "Number", "Email", "CreatedOn", "Amount", "Discount")
+join "Payment" p on p."InvoiceId"::text = o."OrderId"
+where p."State" = 'Accepted'
+and p."GatewayId" = 'stripe'
+""")
+
+    # Run query 2
+    df2 = pd.read_sql(query2, connection_invoicing)
+    df2.to_csv("data/data2date.csv")
+
+    connection_invoicing.close()
+    engine_invoicing.dispose()
+
+    # All orders in a string for the next query
+    all_accepted_orders = '\n'.join(df2['OrderInfo'].astype(str))
+
+    # Remove trailing comma
+    all_accepted_orders = all_accepted_orders[:-1]
+
+    # STEP3 ---------------------------------------
+    # Query n3
+
+    query3 = text(f"""
+-- DB host: 10.10.2.42
+-- DB: witisi-stripe
+
+select  o."OrderId",
+		o."Number",
+		o."Email",
+		o."CreatedOn",
+		o."PaymentId",
+		replace(o."Amount"::text, '.', ',') as "OrderAmount",
+		replace(o."Discount"::text, '.', ',') as "OrderDiscount",
+		p."CurrencyId",
+		p2."IssuerCountry" as "Country",
+		p3."PaymentIntentId",
+		replace(p."Amount"::text, '.', ',') as "PaymentAmount"
+from (values
+-- data begin
+    {all_accepted_orders}
+-- data end
+	 ) as o("OrderId", "Number", "Email", "CreatedOn", "PaymentId", "Amount", "Discount", "PaymentAmount")
+		 join "Payment" p on p."PaymentId"::text = o."PaymentId"
+		 join "CardPayment" p2 on p."PaymentId" = p2."PaymentId"
+		 join "ChargedPayment" p3 on p."PaymentId" = p3."PaymentId"
+""")
+
+    # Run query 2
+    df3 = pd.read_sql(query3, connection_stripe)
+
+    connection_stripe.close()
+    engine_stripe.dispose()
+
+    df3.to_csv("data/data3date.csv", index=False)
+
